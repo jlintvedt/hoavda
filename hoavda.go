ec0518390c55660df4db49edda73c590b58ba1f1
Jonas <jlintvedt@gmail.com>
2017-11-13 10:43:30 +0100

    Add LogRus for logging

--- hoavda.go
+++ hoavda.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"log"
+	"path/filepath"
 
 	"github.com/jlintvedt/bargi/util"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	log.SetLevel(log.DebugLevel)
 	// n := bargi.NewNode()
 	// n.Test()
-	sn, err := util.NewSnapLoader("H:\\Repos\\go\\src\\github.com\\jlintvedt\\testdata")
+
+	path, err := filepath.Abs("..\\testdata")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sn, err := util.NewSnapLoader(path)
 	if err != nil {
 		log.Fatal(err)
 	}
